2ab8fb71454d9c025b183f4ce1498b272b867ae0
Owen <owenmcmonagle@protonmail.com>
2023-12-28 18:41:21 +0000

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,55 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    /**
+     * My attempt at the psychic modelling problem in the 'Algorithm Design Manual 3rd Edition.'
+     * I use a binary counting subset generation method and a typical set cover algorithm with
+     * length qualifier to determine which combinations of lottery tickets will pay off with
+     * a win. Assuming the psychic guarantee is valid... of course.
+     */
     public static void main(String[] args) {
 
-        final var subsets = generateSubsetsFromSet(new int[]{1, 2, 3, 4, 5});
+        final var fixed_lotto_numbers = new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        final var fixed_ticket_size = 6;
+        final var psychic_guarantee = new HashSet<>(Arrays.asList(10, 6, 4, 7));
 
-        for (int i = 0; i < subsets.length; i ++) {
-            for (int x = 0; x < subsets[i].length; x ++) {
-                System.out.print(subsets[i][x] + ", ");
-            }
-            System.out.println();
-        }
+        final var subsets = generateSubsetsFromSet(fixed_lotto_numbers);
+
+        System.out.println("Length of sets: " + subsets.length);
+        final var estimated_minimum_winners = findWinningSetsOfTickets(subsets, psychic_guarantee, fixed_ticket_size);
+
+        System.out.printf("Length of winning sets(%d): %d\n", fixed_ticket_size, estimated_minimum_winners.size());
+        estimated_minimum_winners
+                .forEach(matching -> {
+                    for (Integer integer : matching)
+                        System.out.print(integer + ", ");
+
+                    System.out.println();
+                });
     }
 
+    /**
+     * Set cover algorithm.
+     */
+    private static Set<Set<Integer>> findWinningSetsOfTickets(final Integer[][] total_combos,
+                                                              final Set<Integer> psychic_guarantee,
+                                                              final int fixed_ticket_size) {
+        return Arrays.stream(total_combos)
+                .map(Arrays::asList)
+                .map(HashSet::new)
+                .filter(subset ->
+                        fixed_ticket_size == subset.size() &&
+                        subset.containsAll(psychic_guarantee))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Binary counting subset generator.
+     */
     private static Integer[][] generateSubsetsFromSet(final int[] subset_to_generate_combos_from) {
         // Binary counting method of generating subsets.
 
@@ -47,12 +78,11 @@ public class Main {
             final var unique_combo_to_store_list = new TreeSet<Integer>();
             for ( int x = 0; x < subset_length; x ++) {
 
-                // If we have a base 10 number confirm presence of unique number,
-                // otherwise Zero.
+                // If we have a bit set. Confirm presence of number.
                 if (subsets[i] % 10 == 1)
                     unique_combo_to_store_list.add(subset_to_generate_combos_from[x]);
 
-                subsets[i] = subsets[i] / 10;
+                subsets[i] = subsets[i] / subset_to_generate_combos_from.length;
             }
 
             // use distinct size to create new resized array.
@@ -75,5 +105,13 @@ public class Main {
         final var c2Sum = Arrays.stream(c2).map(i -> "" + i).collect(Collectors.joining());
 
         return c1Sum.compareTo(c2Sum);
+    }
+
+
+    private static int compareSortedCombosStringLength(Integer[] c1, Integer[] c2) {
+        final var c1Sum = Arrays.stream(c1).map(i -> "" + i).collect(Collectors.joining()).length();
+        final var c2Sum = Arrays.stream(c2).map(i -> "" + i).collect(Collectors.joining()).length();
+
+        return Integer.compare(c1Sum, c2Sum);
     }
 }
